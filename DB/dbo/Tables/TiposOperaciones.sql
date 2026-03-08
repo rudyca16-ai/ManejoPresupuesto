27adc95f3821653adcdebab5adec4be7bc3da7c4
rudy_barrios <pasante2@enterprisesolutions.com.py>
2026-03-08 18:11:00 -0300

    prueba, devops

--- DB/dbo/Tables/TiposOperaciones.sql
+++ DB/dbo/Tables/TiposOperaciones.sql
@@ -1,7 +1,6 @@
 ﻿CREATE TABLE [dbo].[TiposOperaciones] (
     [id]          INT           IDENTITY (1, 1) NOT NULL,
     [descripcion] VARCHAR (450) NOT NULL,
-    [prueba]      INT           NULL,
     CONSTRAINT [PK_tipos_operaciones] PRIMARY KEY CLUSTERED ([id] ASC)
 );
 
